d044a6df9aaa866c2d4fc3305e58a6a2386ae570
ehdrjs4502 <ehdrjs4502@naver.com>
2025-09-29 23:07:28 +0900

    chore: api rewrites 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+export const baseUrl =
+  "https://unremunerative-birgit-denticulately.ngrok-free.dev";
+const path = "/api/v1"; // 기본 경로
+
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: `${path}/:path*`,
+        destination: `${baseUrl}${path}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
