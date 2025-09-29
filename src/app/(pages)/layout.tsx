080c5704c6cd2e5fd3f0e21b46d9d43dd06064c3
ehdrjs4502 <ehdrjs4502@naver.com>
2025-09-29 23:08:26 +0900

    chore: react-query provider 설정

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,3 +1,4 @@
+import TanStackProvider from "@/_providers/tanstack";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TanStackProvider>{children}</TanStackProvider>
       </body>
     </html>
   );
