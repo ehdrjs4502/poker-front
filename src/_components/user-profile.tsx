994a8cd73983055a82d114b8fb318963cb384ebd
ehdrjs4502 <ehdrjs4502@naver.com>
2025-10-10 15:49:36 +0900

    feat: 유저 프로필 컴포넌트 구현

---
+++ src/_components/user-profile.tsx
@@ -0,0 +1,24 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+interface UserProfileProps {
+  userName: string;
+  userAvatar?: string;
+}
+
+export default function UserProfile({
+  userName,
+  userAvatar,
+}: UserProfileProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <Avatar>
+        <AvatarImage
+          src={userAvatar || "/images/default-avatar.jpg"}
+          alt={userName}
+        />
+        <AvatarFallback>{userName.charAt(0)}</AvatarFallback>
+      </Avatar>
+      <p className="text-sm font-medium text-white">{userName}</p>
+    </div>
+  );
+}
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/_components/ui/button";
+import UserProfile from "@/_components/user-profile";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <UserProfile userName="John Doe" />
       <Button>Click me</Button>
     </div>
   );
